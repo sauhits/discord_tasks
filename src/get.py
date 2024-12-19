703d28c9e638c34a5e74b9457da8443430058916
Coela <atsushi.obata1207@gmail.com>
2024-12-20 00:09:12 +0900

    add 正規表現を利用してリストで渡したデータからタイトルと締切を抽出するようにした

--- src/get.py
+++ src/get.py
@@ -16,16 +16,15 @@ load_dotenv()
 url = os.getenv("GAKUJO_URL")
 options = Options()
 options.add_argument("--headless")
-
-webdriver_service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=webdriver_service, options=options)
-driver.get(url)
-wait = WebDriverWait(driver, 30)
-
-n = 1.2
+n = 1.5
 
 def getTaskList():
     task_list = []
+    global driver
+    webdriver_service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=webdriver_service, options=options)
+    driver.get(url)
+    wait = WebDriverWait(driver, 30)
     # ログイン
     # 日本語選択
     select_element_locale = driver.find_element(By.ID, "selectLocale")
@@ -67,13 +66,34 @@ def getTaskList():
     wait.until(
         EC.element_to_be_clickable((By.ID, "templateMediumSizeContentsHref"))
     ).click()
+
+    # オプション選択
+    # wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='SC_14002B00_01_SearchConditionForm']/div[1]/ul/li/div/div[2]/div/ul/li[1]/label"))).click()
+    # print("課題区分を選択しました。")
+
+    # wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='SC_14002B00_01_SearchConditionForm']/div[1]/ul/li/div/div[2]/div/a"))).click()
+    # print("詳細選択を開きました。")
+    # sleep(n)
+    # year_select=driver.find_element(By.XPATH, "//*[@id='changeStartYearCmb']")
+    # Select(year_select).select_by_value("2024")
+    # print("2024年度を選択しました。")
+    #
+    # semester_select=driver.find_element(By.ID, "changeStartSemesterCmb")
+    # Select(semester_select).select_by_value("2")
+    # print("後期を選択しました。")
+
+    # wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='SC_14002B00_01_SearchConditionForm']/div[2]/button"))).click()
+    # 課題取得
     task_list = wait.until(
         EC.presence_of_element_located((By.ID, "dataTable01"))
     ).find_elements(By.TAG_NAME, "tr")
     print("課題ページにアクセスしました。")
+
     return task_list
 
 
 def close():
     driver.quit()
     print("ブラウザを閉じました。")
+    
+
