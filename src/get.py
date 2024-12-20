8e55b9f0d758fad768a9ba9328af9a2c418addeb
Coela <atsushi.obata1207@gmail.com>
2024-12-21 00:42:06 +0900

    upgrade try-exceptの追加をした．for-elseによってリトライ処理を実装した．

--- src/get.py
+++ src/get.py
@@ -4,6 +4,7 @@ from time import sleep
 from dotenv import load_dotenv
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.chrome.options import Options
@@ -14,53 +15,58 @@ import totp
 
 load_dotenv()
 url = os.getenv("GAKUJO_URL")
-options = Options()
-options.add_argument("--headless")
+
 n = 1.5
 
 def getTaskList():
-    task_list = []
-    global driver
-    webdriver_service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=webdriver_service, options=options)
-    driver.get(url)
-    wait = WebDriverWait(driver, 30)
-    # ログイン
-    # 日本語選択
-    select_element_locale = driver.find_element(By.ID, "selectLocale")
-    Select(select_element_locale).select_by_value("ja")
-    wait.until(EC.element_to_be_clickable((By.ID, "btnSsoStart"))).click()
-    print("ログインページにアクセスしました。")
-
-    # SSO認証
-    sleep(n)
-    wait.until(EC.presence_of_element_located((By.ID, "i0116"))).send_keys(
-        os.getenv("SSO_USERNAME")
-    )
-    wait.until(EC.element_to_be_clickable((By.ID, "idSIButton9"))).click()
-    sleep(n)
-    wait.until(EC.presence_of_element_located((By.ID, "i0118"))).send_keys(
-        os.getenv("SSO_PASSWORD")
-    )
-    wait.until(EC.element_to_be_clickable((By.ID, "idSIButton9"))).click()
-    print("SSO認証が完了しました。")
-
-    # TOTP
-    totp_key = totp.get_totp_token(os.getenv("OTP_SEC_KEY"))
-    sleep(n)
-    authenticator = wait.until(
-        EC.presence_of_element_located((By.ID, "idTxtBx_SAOTCC_OTC"))
-    )
-    authenticator.send_keys(totp_key)
-    wait.until(
-        EC.presence_of_element_located((By.ID, "idSubmit_SAOTCC_Continue"))
-    ).click()
-    sleep(n)
-    print("二要素認証が完了しました。")
-
-    wait.until(EC.element_to_be_clickable((By.ID, "idBtn_Back"))).click()
-    wait.until(EC.element_to_be_clickable((By.NAME, "_eventId_proceed"))).click()
-    print("ログインが完了しました。")
+    for _ in range(2):
+        task_list = []
+        global driver
+        options = Options()
+        options.add_argument("--headless")
+        # ログイン
+        try:
+            webdriver_service = Service(ChromeDriverManager().install())
+            driver = webdriver.Chrome(service=webdriver_service, options=options)
+            wait = WebDriverWait(driver, 15)
+            driver.get(url)
+            
+            select_element_locale = driver.find_element(By.ID, "selectLocale")
+            Select(select_element_locale).select_by_value("ja")
+            wait.until(EC.element_to_be_clickable((By.ID, "btnSsoStart"))).click()
+            print("ログインページにアクセスしました。")
+            # SSO認証
+            wait.until(EC.presence_of_element_located((By.ID, "i0116"))).send_keys(
+                os.getenv("SSO_USERNAME")
+            )
+            wait.until(EC.element_to_be_clickable((By.ID, "idSIButton9"))).click()
+            wait.until(EC.presence_of_element_located((By.ID, "i0118"))).send_keys(
+                os.getenv("SSO_PASSWORD")
+            )
+            wait.until(EC.element_to_be_clickable((By.ID, "idSIButton9"))).click()
+            print("SSO認証が完了しました。")
+            # TOTP
+            totp_key = totp.get_totp_token(os.getenv("OTP_SEC_KEY"))
+            authenticator = wait.until(
+                EC.presence_of_element_located((By.ID, "idTxtBx_SAOTCC_OTC"))
+            )
+            authenticator.send_keys(totp_key)
+            wait.until(
+                EC.presence_of_element_located((By.ID, "idSubmit_SAOTCC_Continue"))
+            ).click()
+            print("二要素認証が完了しました。")
+            wait.until(EC.element_to_be_clickable((By.ID, "idBtn_Back"))).click()
+            wait.until(EC.element_to_be_clickable((By.NAME, "_eventId_proceed"))).click()
+            print("ログインが完了しました。")
+            break
+        except TimeoutException as te:
+            print(te)
+            close()
+            pass
+        except StaleElementReferenceException as se:
+            print(se)
+            close()
+            pass
 
     # 学情システム内
     wait.until(
@@ -88,12 +94,10 @@ def getTaskList():
         EC.presence_of_element_located((By.ID, "dataTable01"))
     ).find_elements(By.TAG_NAME, "tr")
     print("課題ページにアクセスしました。")
-
     return task_list
 
 
 def close():
     driver.quit()
     print("ブラウザを閉じました。")
-    
 
