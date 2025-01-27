1844623ce18b86698ebdcf58fab638a05517e7c2
Coela <atsushi.obata1207@gmail.com>
2025-01-27 17:59:05 +0900

    rename webdriverの並列化のためにrenameした

---
+++ src/gakujo_format.py
@@ -0,0 +1,60 @@
+import re
+
+test = [
+    "提出物種別\n講義名\n学期/曜日・時限\nタイトル\n状態\n提出期間\n提出状況",
+    "レポート 人工知能概論(1クラス)\n後期後半/火1・2, 後期前半/火1・2 人工知能概論：後半レポート課題 受付中 2025/01/14 00:00 ～ 2025/02/04 23:55 未提出",
+    "レポート コンパイラ(1クラス)\n後期前半/水7・8, 後期後半/水7・8 最終試験レポート 受付中 2025/01/15 08:00 ～ 2025/01/30 00:00 提出済",
+    "レポート スポーツⅡ(情)\n後期前半/金3・4, 後期後半/金3・4 スポーツIIレポート課題 受付中 2025/01/22 00:00 ～ 2025/01/31 17:00 未提出",
+    "小テスト 数理論理Ⅱ(1クラス)\n後期後半/金9・10 第2回 web小テスト 受付中 2025/01/21 17:30 ～ 2025/01/30 23:55 提出済",
+    "レポート ディジタル信号処理(1クラス)\n後期前半/月5・6, 後期後半/月5・6 最終レポート 受付中 2025/01/26 16:30 ～ 2025/02/09 23:55 未提出",
+]
+
+# リストで渡した課題データを[(title, deadline)]の形式に変換する
+def taskFormatter(raw_text: list):
+    # ヘッダーを削除
+    raw_text.pop(0)
+    task_formatted = []
+
+    for task in raw_text:
+        # 半角，全角，タブを削除
+        task = re.sub(r"[\u3000 \t]", "", task)
+        
+        # タイトルの取得
+        title_match = re.match(r"^[^\n]+", task)
+        if title_match:
+            title = title_match.group()
+            # 不要部分を削除
+            title = re.sub(
+                r"^(小テスト|レポート|授業アンケート|学内アンケート|授業評価アンケート)\s*",
+                "",
+                title,
+            )
+            title = re.sub(r"\(.*\)$", "", title)
+        else:
+            print("タイトルが取得できませんでした")
+            continue
+
+        # 締切の取得
+        deadline_match = re.search(r"～(\d{4}/\d{2}/\d{2})(\d{2}:\d{2})", task)
+        if deadline_match:
+            date_str, time_str = deadline_match.groups()
+            month_day = date_str[5:].replace("/", "")
+            time_str = time_str.replace(":", "")
+            deadline_time = int(time_str)
+            
+            # 時刻が23時未満の場合、締切日を前日に変更
+            if deadline_time < 2300:
+                day = int(month_day[2:]) - 1
+                month_day = month_day[:2] + f"{day:02}"
+        else:
+            print("締切が取得できませんでした")
+            continue
+
+        # 結果をリストに追加
+        task_formatted.append((title, f"{month_day} {time_str}"))
+    
+    return task_formatted
+
+# フォーマットを実行して確認
+formatted_tasks = taskFormatter(test)
+print(formatted_tasks)
---
+++ src/gakujo_get.py
@@ -0,0 +1,105 @@
+# coding: UTF-8
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import totp
+from time import sleep
+
+n = 1.5
+
+
+def getTaskList(URL, SSO_USERNAME, SSO_PASSWORD, OTP_SEC_KEY):
+    for _ in range(2):
+        task_list = []
+        global gakujo_driver
+        options = Options()
+        # options.add_argument("--headless")
+        # ログイン
+        try:
+            webdriver_service = Service(ChromeDriverManager().install())
+            gakujo_driver = webdriver.Chrome(service=webdriver_service, options=options)
+            wait = WebDriverWait(gakujo_driver, 15)
+            gakujo_driver.get(URL)
+
+            select_element_locale = gakujo_driver.find_element(By.ID, "selectLocale")
+            Select(select_element_locale).select_by_value("ja")
+            wait.until(EC.element_to_be_clickable((By.ID, "btnSsoStart"))).click()
+            print("ログインページにアクセスしました。")
+            # SSO認証
+            wait.until(EC.presence_of_element_located((By.ID, "i0116"))).send_keys(
+                SSO_USERNAME
+            )
+            wait.until(EC.element_to_be_clickable((By.ID, "idSIButton9"))).click()
+            wait.until(EC.presence_of_element_located((By.ID, "i0118"))).send_keys(
+                SSO_PASSWORD
+            )
+            wait.until(EC.element_to_be_clickable((By.ID, "idSIButton9"))).click()
+            print("SSO認証が完了しました。")
+            # TOTP
+            sleep(2)
+            if len(gakujo_driver.find_elements(By.ID, "idTxtBx_SAOTCC_OTC")) > 0:
+                # totpの認証を行う
+                totp_key = totp.get_totp_token(OTP_SEC_KEY)
+                authenticator = wait.until(
+                    EC.presence_of_element_located((By.ID, "idTxtBx_SAOTCC_OTC"))
+                )
+                authenticator.send_keys(totp_key)
+                wait.until(
+                    EC.presence_of_element_located((By.ID, "idSubmit_SAOTCC_Continue"))
+                ).click()
+                print("二要素認証が完了しました。")
+            else:
+                print("二要素認証はスキップされました。")
+            wait.until(EC.element_to_be_clickable((By.ID, "idBtn_Back"))).click()
+            wait.until(
+                EC.element_to_be_clickable((By.NAME, "_eventId_proceed"))
+            ).click()
+            print("ログインが完了しました。")
+            break
+        except TimeoutException as te:
+            print(te)
+            close()
+            pass
+        except StaleElementReferenceException as se:
+            print(se)
+            close()
+            pass
+
+    # 学情システム内
+    wait.until(
+        EC.element_to_be_clickable((By.ID, "templateMediumSizeContentsHref"))
+    ).click()
+
+    # オプション選択
+    # wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='SC_14002B00_01_SearchConditionForm']/div[1]/ul/li/div/div[2]/div/ul/li[1]/label"))).click()
+    # print("課題区分を選択しました。")
+
+    # wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='SC_14002B00_01_SearchConditionForm']/div[1]/ul/li/div/div[2]/div/a"))).click()
+    # print("詳細選択を開きました。")
+    # sleep(n)
+    # year_select=driver.find_element(By.XPATH, "//*[@id='changeStartYearCmb']")
+    # Select(year_select).select_by_value("2024")
+    # print("2024年度を選択しました。")
+    #
+    # semester_select=driver.find_element(By.ID, "changeStartSemesterCmb")
+    # Select(semester_select).select_by_value("2")
+    # print("後期を選択しました。")
+
+    # wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='SC_14002B00_01_SearchConditionForm']/div[2]/button"))).click()
+    # 課題取得
+    task_list = wait.until(
+        EC.presence_of_element_located((By.ID, "dataTable01"))
+    ).find_elements(By.TAG_NAME, "tr")
+    print("課題ページにアクセスしました。")
+    
+    return task_list
+
+def close():
+    gakujo_driver.quit()
+    print("ブラウザを閉じました。")
